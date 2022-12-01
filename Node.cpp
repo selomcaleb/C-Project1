5283886485b9aa549ca9c0d717be513197f387ce
selomcaleb <111921362+selomcaleb@users.noreply.github.com>
2022-12-01 01:07:15 +0000

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+#include <fstream>
 #include "Route.cpp"
+
 #pragma once
 
 using namespace std;
 
-class Node{
+class Node {
 public:
     shared_ptr<Node> parentNode;
     string airportCode;
@@ -15,65 +17,69 @@ public:
     string airlineCode;
     shared_ptr<vector<Route>> successors;
 
-    Node(shared_ptr<Node> parentNode, string airportCode, int stops, string airLineCode, shared_ptr<vector<Route> > successors){
+    Node(shared_ptr<Node> parentNode, string airportCode, int stops, string airLineCode,
+         shared_ptr<vector<Route> > successors) {
         this->parentNode = parentNode;
         this->airportCode = airportCode;
-        this->stops =stops;
+        this->stops = stops;
         this->airlineCode = airLineCode;
         this->successors = successors;
     }
 
-    shared_ptr<Node> getParentNode(){
+    shared_ptr<Node> getParentNode() {
         return parentNode;
     }
 
-   string getAirportCode(){
+    string getAirportCode() {
         return airportCode;
     }
 
-    int getStops()  {
+    int getStops() {
         return stops;
     }
 
-   string getAirlineCode(){
+    string getAirlineCode() {
         return airlineCode;
     }
 
-    shared_ptr<vector<Route>> getSuccessors(){
+    shared_ptr<vector<Route>> getSuccessors() {
         return successors;
     }
 
-    bool operator==(const Node&  node) {
+    bool operator==(const Node &node) {
         return node.airportCode == this->airportCode && node.airlineCode == this->airlineCode &&
                node.successors == this->successors;
     }
 
-    vector <string> solutionPath(){
+    void solutionPath() {
         vector<string> airportCodes;
         vector<string> airlineCodes;
         vector<string> solution;
         vector<int> stops;
-        Node* thisNode = this;
+        Node *thisNode = this;
 
-        while(thisNode!= nullptr){
+        while (thisNode != nullptr) {
             airportCodes.push_back(thisNode->getAirportCode());
             airlineCodes.push_back(thisNode->getAirlineCode());
             stops.push_back(thisNode->getStops());
             thisNode = thisNode->parentNode.get();
         }
 
-        reverse(airlineCodes.begin(),airlineCodes.end());
-        reverse(airportCodes.begin(),airportCodes.end());
+        reverse(airlineCodes.begin(), airlineCodes.end());
+        reverse(airportCodes.begin(), airportCodes.end());
 
-        for(int i= 0; i < airlineCodes.size()-1;i++){
-            string result = airlineCodes.at(i+1) + " from " + airportCodes.at(i) + " to " +
-                            airportCodes.at(i+1) + " " + to_string(stops.at(i)) + " stops.";
+        for (int i = 0; i < airlineCodes.size() - 1; i++) {
+            string result = airlineCodes.at(i + 1) + " from " + airportCodes.at(i) + " to " +
+                            airportCodes.at(i + 1) + " " + to_string(stops.at(i)) + " stops.";
             solution.push_back(result);
         }
 
-        return solution;
-
-
+        //write the contents of the solution list to a file
+        ofstream myfile("kumasi-winnipeg-output.txt");
+        for (auto & i : solution) {
+            myfile << i << endl;
         }
+        myfile.close();
+    }
 
-    };
+};
