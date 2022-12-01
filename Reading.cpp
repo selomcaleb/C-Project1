1dfd26df52d0bcf62e8582b62834ba60c8b36fb3
selomcaleb <111921362+selomcaleb@users.noreply.github.com>
2022-12-01 01:13:42 +0000

    Update Reading.cpp

--- Reading.cpp
+++ Reading.cpp
@@ -8,10 +8,12 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
+
 using namespace std;
-class Reading{
+
+class Reading {
 public:
-    unordered_map<string, vector<Route>>  readRoutes(){
+    unordered_map<string, vector<Route>> readRoutes() {
         unordered_map<string, vector<Route>> routesMap;
         string readLine;
         ifstream readRouteFile("/Users/selomcaleb/CLionProjects/FINAL C++ PROJECT/routes.csv");
@@ -45,11 +47,11 @@ public:
     };
 
 
-    unordered_map<string, vector<string>> readAirports(){
+    unordered_map<string, vector<string>> readAirports() {
         unordered_map<string, vector<string>> locationToAirportsMap;
         //unordered_map<string,string> airportToPlaces;
         string readLine;
-        ifstream airportReader("/Users/selomcaleb/CLionProjects/test/airports.csv");
+        ifstream airportReader("/Users/selomcaleb/Desktop/LEVEL 200 SEC. SEM/ICP/C-Project1 2/airports.csv");
 
         while (getline(airportReader, readLine)) {
             vector<string> data;
@@ -77,11 +79,10 @@ public:
         return locationToAirportsMap;
     };
 
-    unordered_map<string,string> readAirportAgain() {
+    unordered_map<string, string> readAirportAgain() {
         unordered_map<string, string> airportToPlaces;
         string readLine;
-        ifstream airportReader("/Users/selomcaleb/CLionProjects/test/airports.csv");
-
+        ifstream airportReader("/Users/selomcaleb/Desktop/LEVEL 200 SEC. SEM/ICP/C-Project1 2/airports.csv");
         while (getline(airportReader, readLine)) {
             vector<string> data;
             stringstream iss(readLine);
@@ -101,13 +102,11 @@ public:
 
         }
         airportReader.close();
-        cout<<"ha"<< airportToPlaces.size() <<endl;
         return airportToPlaces;
     };
 
 
-
-    vector<string> breadthFirstSearch(string initialLoc, string destinationLoc){
+    vector<string> breadthFirstSearch(string initialLoc, string destinationLoc) {
 
         deque<shared_ptr<Node>> frontier;
         unordered_set<string> exploredSet;
@@ -115,86 +114,68 @@ public:
         unordered_map<string, vector<string>> locationToAirportsMap = readAirports();
         unordered_map<string, vector<Route>> routesMap = readRoutes();
         vector<string> airports = locationToAirportsMap.at(initialLoc);
-       // unordered_map<string, string> airportsToPlaces = readAirportAgain();
         vector<string> test;
-        //unordered_map<string, string> airportsToPlaces = readAirportAgain();
-//        if(!airports.empty()) cout<<"yes";
 
-        for(auto airport: airports){
-            shared_ptr<Node> airportNode = make_shared<Node>(nullptr,airport,0,"", nullptr);
+        for (auto airport: airports) {
+            shared_ptr<Node> airportNode = make_shared<Node>(nullptr, airport, 0, "", nullptr);
             frontier.push_back(airportNode);
         }
 
-        while (!frontier.empty()){
+        while (!frontier.empty()) {
             shared_ptr<Node> currentNode = frontier.front();
             frontier.pop_front();
             exploredSet.insert(currentNode->getAirportCode());
-            if (currentNode == nullptr){
+            if (currentNode == nullptr) {
                 continue;
             }
 
-            if (routesMap.count(currentNode->getAirportCode())==0) cout<<"not found";
+            if (routesMap.count(currentNode->getAirportCode()) == 0) cout << "not found";
             vector<Route> successors = routesMap.at(currentNode->getAirportCode());
-            if (!successors.empty()){
-                for(Route successor: successors){
-                    shared_ptr<Node> child = make_shared<Node>(currentNode,successor.getDestinationAirportCode(),successor.getStops(),successor.getAirlineCode(),
+            if (!successors.empty()) {
+                for (Route successor: successors) {
+                    shared_ptr<Node> child = make_shared<Node>(currentNode, successor.getDestinationAirportCode(),
+                                                               successor.getStops(), successor.getAirlineCode(),
                                                                nullptr);
-                   cout<<"child"<<child->getAirportCode();
 
-                    string destinationName = airportsToPlaces.at(child->getAirportCode());
-                    if (destinationName=="") cout<<"yes";
-
-                    if (destinationName == destinationLoc){
-                        child->solutionPath();
+                    if (airportsToPlaces.count(child->getAirportCode()) != 0) {
+                        string destinationName = airportsToPlaces.at(child->getAirportCode());
+                        if (destinationName == destinationLoc) {
+                            child->solutionPath();
+                            std::exit(0);
+                        }
+                    } else {
+                        continue;
                     }
 
-                    if (std::find(frontier.begin(),frontier.end(),child) == frontier.end() && exploredSet.find(child->getAirportCode()) == exploredSet.end()){
+                    if (std::find(frontier.begin(), frontier.end(), child) == frontier.end() &&
+                        exploredSet.find(child->getAirportCode()) == exploredSet.end()) {
                         frontier.push_back(child);
                     }
-
                 }
 
             }
-            cout <<"No solution found" << endl;
-            vector<string>empty;
-            return empty;
+
         }
+        vector<string> empty;
+        return empty;
 
     }
 
 
 };
 
-    int main(){
-        ifstream inputFile("/Users/selomcaleb/CLionProjects/FINAL C++ PROJECT/kumasi-winnipeg.txt");
-        string initialLoc;
-        string destinationLoc;
+int main() {
+    ifstream inputFile("/Users/selomcaleb/CLionProjects/FINAL C++ PROJECT/kumasi-winnipeg.txt");
+    string initialLoc;
+    string destinationLoc;
 
 
-        getline(inputFile, initialLoc);
-        getline(inputFile,destinationLoc);
-        ofstream outputFile("kumasi-winnipeg_output.txt");
-        //deque<Node> frontier;
-        //unordered_set<string> exploredSet;
-        //vector<string> airports = locationToAirportsMap.at(initialLoc);
-        Reading bf = Reading();
-        vector<string> path = bf.breadthFirstSearch(initialLoc,destinationLoc);
-        cout<<"path" <<path.size();
-        int numberOfFlights = 0;
-        if(!path.empty()){
-            for (string flight:path){
-                outputFile << flight << endl;
-                numberOfFlights++;
-            }
+    getline(inputFile, initialLoc);
+    getline(inputFile, destinationLoc);
 
-            outputFile <<"Total Flights: " << numberOfFlights;
-        } else{
-            outputFile <<"solution not found";
-        }
-        outputFile.close();
+    Reading bf = Reading();
+    vector<string> path = bf.breadthFirstSearch(initialLoc, destinationLoc);
 
 
-
-        return 0;
-    };
-
+    return 0;
+};
